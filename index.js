423d5cb4435bbc59c6e96470634cd93802825bda
Sebastien Sanz de Santamaria <sebastosh@pm.me>
2019-01-20 19:28:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, days){
+function holidayCountdown(days, holiday){
   return `It\'s ${days} days until ${holiday}!`
 }
